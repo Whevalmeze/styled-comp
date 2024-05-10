12f9368dd6ed579a5408e1166cf2ce42f5365cb4
whevalmeze <princegbefavor@gmail.com>
2024-05-10 22:03:00 +0100

    learn: pseudoclasses

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-  background-color: ${({variant}) => variant == "stroke" ? "#fff" : "green"};
   border-radius: 20px;
+  background-color: ${({variant}) => variant == "stroke" ? "#fff" : "green"};
   color: ${({variant}) => variant == "stroke" ? "green" : "#fff"};
   width: 200px;
   height: 50px;
+  padding: 15px 30px;
+  transition: 0.5s all ease-in-out;
+  &:hover {
+    background-color: ${({variant}) => variant !== "stroke" ? "#fff" : "green"};
+    color: ${({variant}) => variant !== "stroke" ? "green" : "#fff"};
+  }
 `
 export const FancyButton = styled(StyledButton)`
   background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
